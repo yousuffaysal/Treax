6fd3d4720d9d2912fd9d73abaa6e1203111a12a1
YusuF Faisal <yusufoesta7t@gmail.com>
2026-07-27 22:42:42 +0600

    feat: disable user scaling and set maximum scale to 1 in viewport configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   themeColor: [
     { media: '(prefers-color-scheme: light)', color: '#e8ebe6' },
     { media: '(prefers-color-scheme: dark)', color: '#0b0d08' },
